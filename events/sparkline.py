0692251f01484253d814d86b9efd4e87a2a82b43
David Fernández <dfernandez@maldita.es>
2026-02-14 02:07:02 +0000

    fix: avoid requiring TZ tables in mysql by not using TruncHour

--- events/sparkline.py
+++ events/sparkline.py
@@ -1,11 +1,40 @@
 from datetime import timedelta
 from collections import defaultdict
 
-from django.db.models import Count
+from django.db import connection
+from django.db.models import Count, Func, Value, DateField, DateTimeField
 from django.db.models.functions import TruncHour, TruncDate
 from django.utils import timezone
 
 from events.models import Event
+
+
+def _trunc_date(field_name):
+    """TruncDate that works on MySQL without timezone tables loaded.
+
+    On MySQL, Django's TruncDate uses CONVERT_TZ which requires timezone tables to be populated.
+    Since we store and compare in UTC, we can use DATE() directly to avoid this requirement.
+    """
+    if connection.vendor == 'mysql':
+        return Func(field_name, function='DATE', output_field=DateField())
+    return TruncDate(field_name)
+
+
+def _trunc_hour(field_name):
+    """TruncHour that works on MySQL without timezone tables loaded.
+
+    On MySQL, Django's TruncHour uses CONVERT_TZ which requires timezone tables to be populated.
+    Since we store and compare in UTC, we can use DATE_FORMAT + CAST directly.
+    """
+    if connection.vendor == 'mysql':
+        return Func(
+            field_name,
+            Value('%Y-%m-%d %H:00:00'),
+            function='DATE_FORMAT',
+            template="CAST(%(function)s(%(expressions)s) AS DATETIME)",
+            output_field=DateTimeField(),
+        )
+    return TruncHour(field_name)
 
 
 def _get_sparkline_data(filter_field, entity_ids):
@@ -33,7 +62,7 @@ def _get_sparkline_data(filter_field, entity_ids):
     daily_rows = (
         Event.objects
         .filter(**{f'{filter_field}__in': entity_ids, 'digested_at__gte': daily_start, 'digested_at__lt': hourly_cutoff})
-        .annotate(bucket=TruncDate('digested_at'))
+        .annotate(bucket=_trunc_date('digested_at'))
         .values(filter_field, 'bucket')
         .annotate(count=Count('id'))
     )
@@ -42,7 +71,7 @@ def _get_sparkline_data(filter_field, entity_ids):
     hourly_rows = (
         Event.objects
         .filter(**{f'{filter_field}__in': entity_ids, 'digested_at__gte': hourly_cutoff})
-        .annotate(bucket=TruncHour('digested_at'))
+        .annotate(bucket=_trunc_hour('digested_at'))
         .values(filter_field, 'bucket')
         .annotate(count=Count('id'))
     )
